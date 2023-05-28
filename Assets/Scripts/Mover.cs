1cf0013e81b3a6beb3962aabb9e954bdbcf052ad
Dana Sharon <whosedreamisthis@gmail.com>
2023-05-28 10:51:38 -0700

    move player

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -8,13 +8,26 @@ public class Mover : MonoBehaviour
     float xValue = 0.01f;
     void Start()
     {
+        PrintInstructions();
     }
 
     void Update()
     {
+        Move();
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game.");
+        Debug.Log("Use arrows to move player around.");
+        Debug.Log("Don't hit the walls");
+    }
+
+    void Move()
+    {
         float xValue = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
         float zValue = Input.GetAxis("Vertical") * speed * Time.deltaTime;
         transform.Translate(xValue, 0, zValue);
-
     }
+
 }
